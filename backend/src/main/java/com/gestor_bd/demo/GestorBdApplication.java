fb825455488c4adc82597ca06dd23da024d464ae
David G. Escobar <145819431+gioescobar87@users.noreply.github.com>
2024-05-13 20:20:28 -0600

    Adición de archivos de Spring

---
+++ backend/src/main/java/com/gestor_bd/demo/GestorBdApplication.java
@@ -0,0 +1,13 @@
+package com.gestor_bd.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorBdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorBdApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/gestor_bd/demo/GestorBdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestor_bd.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorBdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
