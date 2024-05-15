6d32c6c381e29ac471da3a1848c7b6b43ffb896e
gioescobar87 <descobarl4@miumg.edu.gt>
2024-05-15 17:42:28 -0600

    Creción de controlador para ejecutar sentencias mySQL

---
+++ backend/src/main/java/com/gestor_bd/demo/CreateDBController.java
@@ -0,0 +1,31 @@
+package com.gestor_bd.demo;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+public class CreateDBController {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @PostMapping("/create-database")
+    @Transactional
+    public String createDatabase(@RequestBody String sqlQuery) {
+        try{
+            entityManager.createNativeQuery(sqlQuery).executeUpdate();
+            return "Database created successfully";
+        } catch (Exception e){
+            return "Error creating database "+e.getMessage();
+        }
+    }
+    
+
+}
