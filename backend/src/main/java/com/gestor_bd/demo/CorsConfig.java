7324b26e30e9205d2580f8916683251d844fc3f2
gioescobar87 <descobarl4@miumg.edu.gt>
2024-05-24 23:19:09 -0600

    Configuración CORS

---
+++ backend/src/main/java/com/gestor_bd/demo/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.gestor_bd.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:4200");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+
+}
